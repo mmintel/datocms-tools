3c946fd5cd1e5d7dc04e5f3ce40b8407efcd05af
Marc Mintel <marc@mintel.me>
2019-10-25 13:18:20 +0700

    refactor: export-models uses prompts

---
+++ .bin/export-models.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+const fse = require('fs-extra');
+const prompts = require('prompts');
+const { resolve } = require('path');
+const { exportModels } = require('..');
+
+const questions = [
+  {
+    type: 'text',
+    name: 'apiKey',
+    message: 'Please enter your full-access DatoCMS key',
+    validate: (v) => v.length >= 10,
+  },
+  {
+    type: 'text',
+    name: 'output',
+    message: 'Where would you like to save the file?',
+    initial: './output/models.json',
+  },
+];
+
+const run = async function run() {
+  const response = await prompts(questions);
+  const { apiKey } = response;
+
+  exportModels({
+    apiKey,
+  }).then((data) => {
+    fse.outputFileSync(response.output, JSON.stringify(data));
+  });
+};
+
+run();
